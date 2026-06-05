5217c85c8821a7e2483d1cccff7c9a73cfeff9f1
yasin-arafat-05 <yasinarafat.e2021@gmail.com>
2026-06-05 22:11:07 +0600

    mat_mal_coalesced_version

--- cu/first_program.cu
+++ cu/first_program.cu
@@ -5,12 +5,14 @@
 #include<iostream>
 #include<cuda_runtime.h>
 using namespace std;
+
+// -----------------------------Simple Matrix Multiplication------------------------
 __global__ void simpleMatMulKernel(float *d_M, float *d_N, float *d_P,int width){
     // maping 1D thread to 2D 
     // programmer prefer to map y with row.
     // theory(row,col) => coding(col,row)
-   int  row = blockIdx.y * gridDim.y + threadIdx.y;
-   int  col = blockIdx.x * gridDim.x + threadIdx.x;
+   int  row = blockIdx.y * blockDim.y + threadIdx.y;
+   int  col = blockIdx.x * blockDim.x + threadIdx.x;
    if(row<width && col<width){
         float productValue = 0;
         for(int k=0;k<width;k++){
@@ -22,6 +24,36 @@ __global__ void simpleMatMulKernel(float *d_M, float *d_N, float *d_P,int width)
    }
 }
 
+// -------------------------------Coalesced-MatMul-------------------------
+/*
+let, 
+    - row = 5,
+    - width = 5,
+    k = 1,2,3,4,5,...
+
+    => For d_M[row*width+k] <=
+    row*width+k = 26,27,28,29,30,.....
+
+    => For d_N[k*width+col] <=
+    k*width+col = 10,15,20,25,30,.....
+
+Conclusion: d_M memory access is coalesced but d_M is not.
+*/
+__global__ void coalescedMatMul(float *d_M,float *d_N, float *d_P, int width){
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
+    if(row<width && col<width){
+        float productValue = 0;
+        for(int k=0;k<width;k++){
+            productValue += d_M[row*width+k] * d_N[col*width+k];
+        }
+        d_P[row*width+col] = productValue;
+    }
+
+}
+
+
+// -----------------------Matrix Multiplication in CPU----------------------
 __host__ void matmulOnHost(float *A, float *B,float *ans, int width){
     for(int row=0;row<width;row++){
         for(int col=0;col<width;col++){
@@ -35,6 +67,8 @@ __host__ void matmulOnHost(float *A, float *B,float *ans, int width){
     
 }
 
+
+// ------------------------fillWithRandom Value----------------------------
 __host__ void fillWithRandom(vector<float> &v,long long totalElement){
     // 1. define random seed
     random_device rd;
@@ -52,20 +86,44 @@ __host__ void fillWithRandom(vector<float> &v,long long totalElement){
 
 int main(){
     const int width = 1024;
+    //const int width = 3;
     const long long totalElement = (long long) width*width;
     const size_t size = totalElement * sizeof(float);
 
     cout<<"Allocating Memory: "<< (size*3)/(1024*1024) << "MB Need to allocate from GPU."<<endl;
 
     // allocating memeory on the host device:
+    /*
+
+    // mat1:
+     1 2 3 
+     4 5 6 
+     7 8 9 
+
+    // mat2:
+     9 8 7 
+     6 5 4 
+     3 2 1 
+    
+     // ans: 
+     30 24 18 
+     84 69 54
+     138 114 90
+    */
+
+    // for testing that my kernel funtion is correct.
+    // vector<float> h_M={1,2,3,4,5,6,7,8,9};
+    // vector<float> h_N={9,8,7,6,5,4,3,2,1};
+
+
     vector<float> h_M(totalElement);
     vector<float> h_N(totalElement);
     vector<float> h_P_GPU(totalElement);
     vector<float> h_P_CPU(totalElement);
 
     // fill elemnet with rm number:
-    fillWithRandom(h_M,totalElement);
-    fillWithRandom(h_N,totalElement);
+    // fillWithRandom(h_M,totalElement);
+    // fillWithRandom(h_N,totalElement);
 
     // allocate memery in gpu:
     float *d_M, *d_N, *d_P;
@@ -74,8 +132,8 @@ int main(){
     cudaMalloc(&d_P,size);
 
     // copy from host to device:
-    cudaMemcpy(&d_M,h_M.data(),size,cudaMemcpyHostToDevice);
-    cudaMemcpy(&d_N,h_N.data(),size,cudaMemcpyHostToDevice);
+    cudaMemcpy(d_M,h_M.data(),size,cudaMemcpyHostToDevice);
+    cudaMemcpy(d_N,h_N.data(),size,cudaMemcpyHostToDevice);
 
     // define block and grid:
     dim3 threadPerBlock(16,16,1);
@@ -100,6 +158,42 @@ int main(){
     // copy result back to device to host:
     cudaMemcpy(h_P_GPU.data(),d_P,size,cudaMemcpyDeviceToHost);
     cout<<"Multiplication is on GPU complete"<<endl;
+    // Print results
+    // cout << "\n=== GPU Result ===\n";
+    // for(int i = 0; i < width; i++){
+    //     for(int j = 0; j < width; j++){
+    //         cout << h_P_GPU[i*width + j] << "\t";
+    //     }
+    //     cout << endl;
+    // }
+
+    // ----------------For coalesced mat mul---------------
+    // transpose the matrix:
+    vector<float> h_N_coal(totalElement);
+    for(int i=0;i<width;i++){
+        for(int j=0;j<width;j++){
+            h_N_coal[j*width+i] = h_N[i*width+j];
+        }
+    }
+    cudaMemcpy(d_N,h_N_coal.data(),size,cudaMemcpyHostToDevice);
+    start = chrono::high_resolution_clock::now();
+    coalescedMatMul<<<blockPerGrid,threadPerBlock>>>(d_M,d_N,d_P,width);
+    stop = chrono::high_resolution_clock::now();
+    duration = stop - start;
+    cout<<"Time taken in GPU (coalesced version): "<< duration.count()<<endl; 
+    
+    // copy result back to device to host:
+    cudaMemcpy(h_P_GPU.data(),d_P,size,cudaMemcpyDeviceToHost);
+    cout<<"Multiplication is on GPU complete: coalesced version"<<endl;
+    // Print results
+    // cout << "\n=== GPU Result ===\n";
+    // for(int i = 0; i < width; i++){
+    //     for(int j = 0; j < width; j++){
+    //         cout << h_P_GPU[i*width + j] << "\t";
+    //     }
+    //     cout << endl;
+    // }
+
 
     // Clean the GPU:
     cudaFree(d_M);
@@ -113,14 +207,13 @@ int main(){
     auto end1 = chrono::high_resolution_clock::now();
     chrono::duration<float,milli> duration1 = end1 - start1;
     cout<<"Time taken in CPU: "<<duration1.count()<<endl;
+    // cout << "\n=== CPU Result ===\n";
+    // for(int i = 0; i < width; i++){
+    //     for(int j = 0; j < width; j++){
+    //         cout << h_P_CPU[i*width + j] << "\t";
+    //     }
+    //     cout << endl;
+    // }
 }
 
-/*
-===========Output==========
-first_program.cu
-tmpxft_00000120_00000000-10_first_program.cudafe1.cpp
-Allocating Memory: 12MB Need to allocate from GPU.
-Time taken in GPU: 66.9367
-Multiplication is on GPU complete
-Time taken in CPU: 3283.21
-*/
+
